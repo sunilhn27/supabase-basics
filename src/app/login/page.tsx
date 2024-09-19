5c47ad61bd44cb1ba3d4ab70649d345b235a1f18
Sunil H N <sunilh.n27@gmail.com>
2024-09-19 23:52:38 +0530

    added login page

---
+++ src/app/login/page.tsx
@@ -0,0 +1,12 @@
+import { LoginPage } from "@/components/login-page";
+import React from "react";
+
+function Login() {
+  return (
+    <div>
+      <LoginPage />
+    </div>
+  );
+}
+
+export default Login;
